9f85b407b7cad7d6fd893eba0a64b50e0ccb852d
vishal7732 <58137166+vishal7732@users.noreply.github.com>
2020-01-19 16:43:24 +0530

    Create Create list from user input.py

---
+++ small_python codes/For Loop/Create list from user input.py
@@ -0,0 +1,17 @@
+q = []
+c = 1
+for i in range(5):
+    a = int(input(f"No. {c} : "))
+    q.append(a)
+    c +=1
+print(q)
+
+
+
+Output:
+No. 1 : 1
+No. 2 : 2
+No. 3 : 3
+No. 4 : 4
+No. 5 : 5
+[1, 2, 3, 4, 5]
