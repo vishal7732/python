9951a54bcfbb3bd4706a528addd7652c52539349
vishal7732 <58137166+vishal7732@users.noreply.github.com>
2020-01-19 17:40:55 +0530

    Create Create multiplication Table.py

---
+++ small_python codes/while loop/Create multiplication Table.py
@@ -0,0 +1,22 @@
+a = int(input("Enter a Number : "))
+b = 1
+while b < 11:
+    c = a * b
+    b += 1
+    print(f"{a} X {b} = {c}")
+
+
+
+
+Output:
+Enter a Number : 9
+9 X 2 = 9
+9 X 3 = 18
+9 X 4 = 27
+9 X 5 = 36
+9 X 6 = 45
+9 X 7 = 54
+9 X 8 = 63
+9 X 9 = 72
+9 X 10 = 81
+9 X 11 = 90
